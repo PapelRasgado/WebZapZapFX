16b4d365f26fc94b5d85bfca5320a00d0386b02a
PapelRasgado <joaodantas31.jpd@gmail.com>
2018-10-29 14:56:02 -0300

    Teoricamente finalizado

--- src/zapzap/main/Agendador.java
+++ src/zapzap/main/Agendador.java
@@ -4,9 +4,11 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.TimerTask;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByLinkText;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -37,15 +39,25 @@ public class Agendador extends TimerTask {
 					try {
 						Thread.sleep(1000);
 						List<WebElement> text = driver.findElements(By.className("_2S1VP"));
-						if(driver.findElements(By.className("_1WZqU")).size()>0) {
-							System.out.println("Numero invalido");
-							break;
+						try {
+							WebElement aviso = driver.findElement(By.xpath("html/body/div/div/span[3]/div/span/div/div/div/div/div/div"));
+							if (aviso != null) {
+								if (aviso.getText().contains("url é inválido")) {
+									System.out.println(driver
+											.findElement(
+													By.xpath("html/body/div/div/span[3]/div/span/div/div/div/div/div/div"))
+											.getText());
+									break;
+								}
+							}
+						} catch (Exception ex) {
+							System.out.println("ta carregando irmão calma ai");
 						}
 						if (text.size() > 0) {
 							text.get(0).sendKeys(cliente.getMessage());
 							text.get(0).sendKeys(Keys.ENTER);
 							Thread.sleep(5000);
-							mainApp.getClienteData().remove(cliente);
+							// mainApp.getClienteData().remove(cliente);
 							break;
 						}
 					} catch (Exception e) {
