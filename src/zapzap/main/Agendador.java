f2319cfbde09895f720b7ea69fb717c9a56c7d46
PapelRasgado <joaodantas31.jpd@gmail.com>
2018-11-17 11:46:17 -0200

    Comentarios adicionados

--- src/zapzap/main/Agendador.java
+++ src/zapzap/main/Agendador.java
@@ -34,10 +34,17 @@ import zapzap.main.MainApp;
 public class Agendador extends TimerTask {
 
 	private MainApp mainApp;
+
+	// Controlador principal, utilizado aqui para obter os dados.
 	private WebDriver driver;
+
+	// Série de ids utilizados para localizar os objetos com o selenium, todos eles
+	// sao obtidos usando o firebase
 	private String idInvalido;
 	private String idEdit;
 	private String idConexao;
+
+	private String emails;
 
 	public Agendador(MainApp mainApp, WebDriver driver) {
 		this.mainApp = mainApp;
@@ -48,86 +55,135 @@ public class Agendador extends TimerTask {
 	public void run() {
 		Firebase database;
 		try {
+			// Tenta criar conexão com o firebase
 			database = new Firebase("https://webzapzap-ade09.firebaseio.com/");
+
+			// Identificador do aviso de que o numero é invalido
 			String id = database.get("id-invalido").getRawBody();
 			idInvalido = id.substring(1, id.length() - 1);
+
+			// Identificador de edit utilizado para inserir o texto
 			id = database.get("id-edit").getRawBody();
 			idEdit = id.substring(1, id.length() - 1);
+
+			// Identificador do aviso de que a conexão com o celular foi perdida
 			id = database.get("id-conexao").getRawBody();
 			idConexao = id.substring(1, id.length() - 1);
+
+			// Pega do banco os emails dos desenvolvedores para alertalos caso ocorra algum
+			// erro que necessite manutenção manual
+			String emails = database.get("emails").getRawBody();
+			this.emails = emails.substring(1, emails.length() - 1);
+			
 		} catch (FirebaseException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("Não começou");
+
+		// Puxa a lista de dados do mainApp
 		ArrayList<Cliente> clientes = new ArrayList<Cliente>(mainApp.getClienteData());
+
 		loop: for (Cliente cliente : clientes) {
+			// Teste verificando a hora, se já passou das 18h ele para a execução
 			if (Calendar.getInstance().get(Calendar.HOUR_OF_DAY) >= 18) {
 				break;
 			}
+
+			// Contador que representa o numero de testes em localizar o edit que a mensagem
+			// vai ser inserida
 			int contador = 0;
+
+			// Calcula a quantidade de dias que falta para a conta do cliente vencer, se for
+			// menos que 5 dias ele envia a mensagem
 			long dias = ChronoUnit.DAYS.between(LocalDate.now(), cliente.getData());
-			System.out.println(cliente.getName() + " " + dias);
 			if (dias <= 5) {
+
+				// Aqui ele abre a converso do usuario no driver selenium
 				driver.get("https://web.whatsapp.com/send?phone=+55" + cliente.getNumber());
+
+				// Loop infinito proposital, que só é quebrado quando a mensagem é enviada, ou
+				// quando algum erro é detectado
 				while (true) {
 					try {
-						System.out.println("1");
+						// Tempo de espera para que a execução não fique muito pesada
 						Thread.sleep(1000);
+
+						// Porcura o aviso de conversa inexistente, ou seja, o número fornecido é
+						// invalido ou não possui whatsapp
 						WebElement aviso = driver.findElement(By.xpath(idInvalido));
+						// Caso não encontre ele retorna null
 						if (aviso != null) {
 							if (aviso.getText().contains("url é inválido")) {
+								// Nesse ponto ele retira o cliente da lista de agendamento e adiciona na de
+								// falhas
 								mainApp.getClienteFailData().add(cliente);
 								mainApp.getClienteData().remove(cliente);
-								System.out.println("3");
 								break;
 							}
 						}
 
-						System.out.println("2");
+						// Caso não encontre o aviso de falha, ele procura o edit para inserir a
+						// mensagem
 						WebElement text = driver.findElement(By.className(idEdit));
+						// Caso não encontre ele retorna null
 						if (text != null) {
-							System.out.println("6");
-							Thread.sleep(5000);
+							// Espera 3 segundos para garantir que a tela de carregamento já passou
+							Thread.sleep(3000);
 							text.sendKeys(cliente.getMessage());
 							text.sendKeys(Keys.ENTER);
 							mainApp.getClienteData().remove(cliente);
 							break;
 						}
 					} catch (NoSuchElementException e) {
+						// Em caso de não achar nenhum dos dois elemento ele contabiliza uma falha, ao
+						// acumular 200 um alerta é emitido
+						contador++;
 						if (contador == 200) {
 							contador = 0;
+							// Quando ele verifica as falhas e reseta elas, procura saber o motivo, se
+							// encontrar o aviso de conexão perdida exibe um alerta para o usuario tentar
+							// reestabelecer conexão entre o celular e o webwhats
 							WebElement aviso = driver.findElement(By.className(idConexao));
 							if (aviso != null) {
 								Alert alert = new Alert(AlertType.WARNING);
 								alert.setTitle("Erro ao enviar mensagem");
 								alert.setHeaderText("Verifique se o celular esta conectado");
-								System.out.println("5");
+
+								// Ao confirmar o aviso a execução volta ao normal
 								alert.showAndWait();
 							} else {
+								// Caso não encontre o aviso, o possivel erro está nos identificadores, que
+								// possivelmente foram trocas pela equipe do whatsaap, quando isso acontece um
+								// email é enviado para os desenvolvedores, para que eles atualizem no firebase
 								sendMail();
-								Alert alert = new Alert(AlertType.WARNING);
-								alert.setTitle("Os identificadores de elemento do WhatsApp/nforam atualizas");
-								alert.setHeaderText(
-										"Os desenvolvedores já foram avisados e estão/ncorrigindo o problema!");
 
+								Alert alert = new Alert(AlertType.WARNING);
+								alert.setTitle(
+										"Os identificadores de elemento do WhatsApp/n possivelmente foram atualizas");
+								alert.setHeaderText(
+										"Os desenvolvedores já foram avisados e estão/n tentando corrigir o problema!");
+								// Ao confirmar esse aviso a execução dos envios é parada, já que sem os novos
+								// identificares é impossivel enviar, logo, para não gerar mais erros o loop é
+								// quebrado
 								alert.showAndWait();
 								break loop;
 							}
 
 						}
-						contador++;
 					} catch (Exception e) {
-						System.out.println();
+						e.printStackTrace();
 					}
 				}
 			}
 		}
 	}
 
+	/**
+	 * Método copiado do link
+	 * https://www.devmedia.com.br/enviando-email-com-javamail-utilizando-gmail/18034
+	 * com algumas adaptações
+	 */
 	private void sendMail() {
 		Properties props = new Properties();
 
@@ -139,6 +195,7 @@ public class Agendador extends TimerTask {
 
 		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
+				// Email e senha do email criado para utilizar firebase e enviar os emails
 				return new PasswordAuthentication("webzapdantas@gmail.com", "d4n7a52018");
 			}
 		});
@@ -149,7 +206,7 @@ public class Agendador extends TimerTask {
 			message.setFrom(new InternetAddress("webzapdantas@gmail.com")); // Remetente
 
 			Address[] toUser = InternetAddress // Destinatário(s)
-					.parse("eniedson.silva@gmail.com, joaodantas31.jpd@gmail.com");
+					.parse(emails);
 
 			message.setRecipients(Message.RecipientType.TO, toUser);
 			SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
--- src/zapzap/main/MainApp.java
+++ src/zapzap/main/MainApp.java
@@ -40,16 +40,19 @@ public class MainApp extends Application {
 	private Stage primaryStage;
 	private BorderPane rootLayout;
 	private MainViewController mainViewController;
-	
-	//Url para o local aonde o arquivo com as informações são salvos.
+
+	// Url para o local aonde o arquivo com as informações são salvos.
 	private String url = System.getProperty("user.home") + "//Documents//Webzapzap";
-	
-	//Driver do Selenium utilizado para manipular o whatsweb
+
+	// Driver do Selenium utilizado para manipular o whatsweb
 	private WebDriver driver;
 
+	// Lista de clientes agendados
 	private ObservableList<Cliente> clienteData = FXCollections.observableArrayList();
+
+	// Lista de falhas durante o envio da mensagem
 	private ObservableList<Cliente> clienteFailData = FXCollections.observableArrayList();
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
@@ -61,7 +64,7 @@ public class MainApp extends Application {
 
 		showClientOverview();
 	}
-	
+
 	public ObservableList<Cliente> getClienteFailData() {
 		return clienteFailData;
 	}
@@ -74,11 +77,20 @@ public class MainApp extends Application {
 		driver.get("https://web.whatsapp.com/");
 		new Thread() {
 			public void run() {
-				
+
 				try {
+
+					// As informações de ids são pegos do firebase, para que possamos alterar
+					// durante o tempo de execução, mesmo não tendo acesso a aplicação diretamente.
 					Firebase database = new Firebase("https://webzapzap-ade09.firebaseio.com/");
 					String id = database.get("id-qr").getRawBody();
-					id = id.substring(1, id.length()-1);
+
+					// A string vem entre aspas, por isso é necessario quebrar ela.
+					id = id.substring(1, id.length() - 1);
+
+					// Loop que mantam o aplicativo parado até que o usuario escanei o qr-code do
+					// whats
+					// A cada 1 segundo ele repete a busca, até não achar mais o qr-code ele passa.
 					while (true) {
 						List<WebElement> elems = driver.findElements(By.className(id));
 						if (elems.size() == 0) {
@@ -96,40 +108,45 @@ public class MainApp extends Application {
 				} catch (UnsupportedEncodingException e1) {
 					e1.printStackTrace();
 				}
-				
+
 			};
 		}.start();
-		
-		
-		Timer timer = new Timer();
-        Agendador agendador = new Agendador(this, driver);
-        Calendar data = Calendar.getInstance();
-        if(data.get(Calendar.HOUR_OF_DAY) > 12) {
-        	data.add(Calendar.DATE, 1);
-        }
-        data.set(Calendar.HOUR_OF_DAY, 12);
-        data.set(Calendar.MINUTE, 00);
-        data.set(Calendar.SECOND, 0);
-        timer.schedule(agendador, data.getTimeInMillis()-Calendar.getInstance().getTimeInMillis(), 86400000);
-		
-	}
 
+		// Utilizamos a bibioteca Timer para agendar as mensagens, sempre ao meio dia
+		// ele comeca a enviar as mensagens, e para as 18 horas.
+		Timer timer = new Timer();
+		Agendador agendador = new Agendador(this, driver);
+		Calendar data = Calendar.getInstance();
+		if (data.get(Calendar.HOUR_OF_DAY) > 12) {
+			data.add(Calendar.DATE, 1);
+		}
+		data.set(Calendar.HOUR_OF_DAY, 12);
+		data.set(Calendar.MINUTE, 00);
+		data.set(Calendar.SECOND, 0);
+
+		// Apos agendado é dado um tempo de repetição de 86400 segundos, ou seja, um dia
+		timer.schedule(agendador, data.getTimeInMillis() - Calendar.getInstance().getTimeInMillis(), 86400000);
+
+	}
 
 	public ObservableList<Cliente> getClienteData() {
 		return clienteData;
 	}
 
+	/**
+	 * Método que inicializa o layout root, aonde os outros layouts serão inseridos.
+	 * 
+	 */
 	public void initRootLayout() {
 		try {
-			
+
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainApp.class.getResource("view/RootView.fxml"));
 			rootLayout = (BorderPane) loader.load();
 
 			RootViewController controller = loader.getController();
 			controller.setMainApp(this);
-			
-			
+
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -138,7 +155,12 @@ public class MainApp extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
+	/**
+	 * Método que insere o princiapl view, que possui as opções de adicionar e
+	 * visulizar cliente, no layout root.
+	 * 
+	 */
 	public void showClientOverview() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
@@ -163,22 +185,30 @@ public class MainApp extends Application {
 		launch(args);
 	}
 
-	public boolean showClienteDetails(Cliente cliente, boolean teste) {
+	/**
+	 * Método que exibe um view dialog com as informações do cliente
+	 * 
+	 * @param cliente cliente que terá suas informações exibidas
+	 * @param tipo    tipo se refere ao que está sendo exibido(true:cliente,
+	 *                false:falha)
+	 * 
+	 */
+	public boolean showClienteDetails(Cliente cliente, boolean tipo) {
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainApp.class.getResource("view/DetailsDialog.fxml"));
 			AnchorPane page = (AnchorPane) loader.load();
 
 			Stage dialogStage = new Stage();
-			if (teste) {
+			if (tipo) {
 				dialogStage.setTitle("Detalhes Cliente");
 			} else {
 				dialogStage.setTitle("Detalhes Falha");
 			}
-			
+
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.initOwner(primaryStage);
-			
+
 			Scene scene = new Scene(page);
 			dialogStage.setScene(scene);
 			dialogStage.getIcons().add(new Image("file:resources/images/logo.png"));
@@ -187,17 +217,20 @@ public class MainApp extends Application {
 			controller.setDialogStage(dialogStage);
 			controller.setCliente(cliente);
 			controller.setMainApp(this);
-			controller.setTipo(teste);
+			controller.setTipo(tipo);
 
 			dialogStage.showAndWait();
 
 			return true;
-		}catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 			return false;
 		}
 	}
 
+	/**
+	 * Método encarregado de persistir os dados localmente.
+	 */
 	public void save() {
 		try {
 
@@ -208,24 +241,27 @@ public class MainApp extends Application {
 			oos.writeObject(new ArrayList<Cliente>(clienteData));
 
 			oos.close();
-			
+
 			fout = new FileOutputStream(url + "/savefail.ser");
-			
+
 			ObjectOutputStream oosFail = new ObjectOutputStream(fout);
-			
+
 			oosFail.writeObject(new ArrayList<Cliente>(clienteFailData));
-			
+
 			oosFail.close();
 
-		} catch(FileNotFoundException f) {
+		} catch (FileNotFoundException f) {
 			File diretorio = new File(url);
-            diretorio.mkdir();
+			diretorio.mkdir();
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
 
 	@SuppressWarnings("unchecked")
+	/**
+	 * Método encarregado de restaurar os dados localmente
+	 */
 	public void read() {
 		try {
 
@@ -235,16 +271,16 @@ public class MainApp extends Application {
 
 			List<Cliente> list = (List<Cliente>) ois.readObject();
 			clienteData = FXCollections.observableList(list);
-			
+
 			ois.close();
-			
+
 			fin = new FileInputStream(url + "/savefail.ser");
-			
+
 			ObjectInputStream oisFail = new ObjectInputStream(fin);
 
 			List<Cliente> listFail = (List<Cliente>) oisFail.readObject();
 			clienteFailData = FXCollections.observableList(listFail);
-			
+
 			oisFail.close();
 
 		} catch (FileNotFoundException ex) {
@@ -254,19 +290,25 @@ public class MainApp extends Application {
 		}
 	}
 
-
-	
 	@Override
 	public void stop() throws Exception {
 		save();
-		System.exit(0);
 		driver.close();
+		System.exit(0);
+
 		super.stop();
 	}
 
+	/**
+	 * Esté método é encarregado de fazer a ligação entre o dialogController e o
+	 * mainControler, para que possa ocorrer a edição de objetos
+	 * 
+	 * @param cliente cliente a ser editado
+	 * @param tipo    tipo do objeto a ser editado(true:cliente, false:falha)
+	 */
 	public void editar(Cliente cliente, boolean tipo) {
 		mainViewController.editar(cliente, tipo);
-		
+
 	}
-	
+
 }
